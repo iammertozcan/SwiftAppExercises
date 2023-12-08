7ed24a31f6036561e6530b1e1c9837deac0bfa62
Mert Özcan <130929421+mxrtxzcxn@users.noreply.github.com>
2023-12-08 15:22:13 +0300

    Add files via upload

---
+++ 1. PhotoFrame/PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by Mert Özcan on 8.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
